c9eb29727eb0b8daffaa8001900abd10af82e4bc
Lucas Israel <lisraelalves92@gmail.com>
2022-05-21 01:09:18 -0300

    perguntar sobre parseInt radix do desafio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,8 +12,19 @@ function triangleCheck(a, b, c) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(frase) {
+  let reg = /\d/g;
+  let numeros = frase.match(reg);
+  let sum = 0;
+  for (let i in numeros) {
+    if (numeros[i] > 0 && numeros[i] < 10) {
+      sum += parseInt(numeros[i], 10);
+    }
+  }
+  if (sum < 2) {
+    return `${sum} copo de água`;
+  }
+  return `${sum} copos de água`;
 }
 
 module.exports = {
