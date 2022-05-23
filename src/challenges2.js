9553825fa739ccce5d321c1a878e16569c509917
Lucas Israel <lisraelalves92@gmail.com>
2022-05-23 14:00:57 -0300

    desafio 11 preciso arrumar complexidade

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,43 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function isInvalid1(arr) {
+  for (let index = 0; index < arr.length; index += 1) {
+    if (arr[index] > 9 || arr[index] < 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isInvalid2(arr) {
+  for (let index = 0; index < arr.length; index += 1) {
+    let repete = 0;
+    for (let index2 = 0; index2 < arr.length; index2 += 1) {
+      if (arr[index] === arr[index2]) {
+        repete += 1;
+      }
+    }
+    if (repete > 2) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function generatePhoneNumber(arr) {
+  let formato = '(xx) xxxxx-xxxx';
+
+  if (arr.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (isInvalid1(arr) === false || isInvalid2(arr) === false) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  if (arr.length === 11) {
+    arr.forEach((numero) => {
+      formato = formato.replace('x', numero);
+    });
+    return formato;
+  }
 }
 
 // Desafio 12
